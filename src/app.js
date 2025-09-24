f9e56d2052d3d71e0c2d343fc91670c231f26f4c
Daniel-Giraldo105548 <dgiraldog105548@umanizales.edu.co>
2025-09-24 18:00:47 -0500

    Decimo commit - subida inicial del proyecto Node.js

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
    Servir archivos estáticos
    ------------------------- */
 // Sirve cualquier archivo dentro de la carpeta 'publica'
+app.use(express.static(path.join(__dirname, '../publica')));
 
 // Ruta raíz: devuelve tu HTML específico
 app.get('/', (req, res) => {
