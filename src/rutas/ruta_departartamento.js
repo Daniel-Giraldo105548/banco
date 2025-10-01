22325b0eda2cfd460f53cb842b49b715c20adbad
Daniel-Giraldo105548 <dgiraldog105548@umanizales.edu.co>
2025-10-01 15:18:22 -0500

    34 commit

--- src/rutas/ruta_departartamento.js
+++ src/rutas/ruta_departartamento.js
@@ -6,7 +6,7 @@ const controlador_departamento = require('../controladores/controlador_departame
 // Rutas para departamento
 enrutador.post('/registrar', controlador_departamento.registrarDepartamento);
 enrutador.get('/listar', controlador_departamento.listarDepartamento);
-enrutador.put('/actualizar/:id_corresponsal', controlador_departamento.actualizarDepartamento);
-enrutador.delete('/borrar/:id_corresponsal', controlador_departamento.borrarDepartamento);
+enrutador.put('/actualizar/:id_departamento', controlador_departamento.actualizarDepartamento);
+enrutador.delete('/borrar/:id_departamento', controlador_departamento.borrarDepartamento);
 
 module.exports = enrutador;
